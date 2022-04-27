ce7058c5b8f8d3df7947151f8067785e42803cb0
Thieffen Delabaere <thieffen.delabaere@ext.ec.europa.eu>
2022-04-27 09:32:19 +0200

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `ech-survey-static`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
